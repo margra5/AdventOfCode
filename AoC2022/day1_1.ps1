d06095a5390c58e19c6069923cbecae1e0ec66f8
Marcus Granberg <marcus.granberg@iver.se>
2022-12-16 21:43:24 +0100

    It works

--- AoC2022/day1_1.ps1
+++ AoC2022/day1_1.ps1
@@ -1,5 +1,15 @@
-$inData = @()
+[int[]]$inData = Get-Content -Path '.\inputFiles\day1_1.txt'
 
-$inData = Get-Content -Path '.\inputFiles\day1_1.txt'
-
-$inData.Count
+foreach ($data in $indata) {
+    if ($data -ne 0) {
+        $cal += $data
+    }
+    else {
+        if ($cal -gt $result) {
+            $result = $cal
+            $cal = 0
+        }
+        $cal = 0
+    }
+}
+$result
