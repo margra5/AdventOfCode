ab418f4f359c05d967498a7949c67f91feb687dc
Marcus Granberg <marcus.granberg@iver.se>
2022-12-21 18:09:54 +0100

    hmmm

--- AoC2022/day2_1.ps1
+++ AoC2022/day2_1.ps1
@@ -1,28 +1,14 @@
 $inData = Get-Content -Path '.\inputFiles\day2_1.txt'
-$score
 
-$inData = $inData.Replace("A", 1)
-$inData = $inData.Replace("B", 2)
-$inData = $inData.Replace("C", 3)
-
-$inData = $inData.Replace("X", 1)
-$inData = $inData.Replace("Y", 2)
-$inData = $inData.Replace("Z", 3)
-
-[int[]]$inData
-
-foreach ($data in $inData) {
-    [int[]]$value = $data.Split()
-    if ($value[0] -eq $value[1]) {
-        $score += 3
-    }
-    elseif ($value[0] -lt $value[1]) {
-        $score += 6
-    }
-    $score += $value[1]
-}
-
-# Är inte 15013
-# Är inte 5272
-
-$score
+$inData | ForEach-Object {
+    @{
+     'A X' = 3 + 1  # draw, rr
+     'A Y' = 6 + 2  # win,  rp
+     'A Z' = 0 + 3  # loss, rs
+     'B X' = 0 + 1  # loss, pr
+     'B Y' = 3 + 2  # draw, pp
+     'B Z' = 6 + 3  # win,  ps
+     'C X' = 6 + 1  # win,  sc
+     'C Y' = 0 + 2  # loss, sp
+     'C Z' = 3 + 3  # draw, ss
+    }[$_]} | Measure-Object -Sum
