21233b2645df1fecce407df9aa5780d4f0f712ed
marcus <marcus.granis@gmail.com>
2023-12-01 12:51:58 +0100

    removed test and added file for day1_2

--- AoC2023/python/day1_1.py
+++ AoC2023/python/day1_1.py
@@ -4,15 +4,8 @@ with open("inputFiles\input_day_1") as f:
     #res = [eval(f) for f in f]
     f = f.read().splitlines()
 
-test_case = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
-
 tot = ""
 tot_num = 0
-"""
-for v in test_case:
-    for i in v:
-        print(i)
-"""
 
 for v in f:
     first_num = re.findall(r'\d', v)[0]
